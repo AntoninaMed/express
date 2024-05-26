b9b0cd2e977e590adaefae91878642519c407285
AntoninaMed <ton.kraski2427@gmail.com>
2024-05-26 20:28:47 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 
 import express from 'express';
-
-const app = express();
 const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+// наприклад, у запитах POST або PATCH
+app.use(express.json());
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
